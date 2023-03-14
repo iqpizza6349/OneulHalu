0c7f6434dbd05df26cda778a30c2234d82c35ddf
iqpizza6349 <iqpizza6349@gmail.com>
2023-03-14 11:29:07 +0900

    :tada: :: first commit

---
+++ oneul-halu-server/src/main/java/me/iqpizza6349/oneulhaluserver/OneulHaluServerApplication.java
@@ -0,0 +1,13 @@
+package me.iqpizza6349.oneulhaluserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulHaluServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneulHaluServerApplication.class, args);
+    }
+
+}
---
+++ oneul-halu-server/src/test/java/me/iqpizza6349/oneulhaluserver/OneulHaluServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iqpizza6349.oneulhaluserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneulHaluServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
