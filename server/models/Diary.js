0e2b3ae12feed6eb3beb29153573635e4debab74
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-25 20:20:34 +0900

    :construction: :: create Diary model class

---
+++ server/models/Diary.js
@@ -0,0 +1,12 @@
+
+class Diary {
+    constructor(diaryNo, authorId, content, emoji, wroteDate) {
+        this.diaryNo = diaryNo;
+        this.authorId = authorId;
+        this.content = content;
+        this.emoji = emoji;
+        this.wroteDate = wroteDate;
+    }
+}
+
+module.exports = Diary;
