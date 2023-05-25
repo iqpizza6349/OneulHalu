c21b6675fa964fbdc7cb86319ce0d4e8422389a3
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-25 20:24:42 +0900

    :recycle: :: refactor controller codes from spring to express

---
+++ server/controllers/diary.js
@@ -0,0 +1,90 @@
+const { findAllByAuthor, findDiary, save, findByNo, exists, update, deleteByNo } = require('../dao/diary');
+
+const { v4 } = require('uuid');
+
+exports.diaries = async (req, res, next) => {
+    const member = res.locals.decoded;
+
+    const year = parseInt(req.query.year, 10);
+    const month = parseInt(req.query.month, 10) - 1;
+
+    const end = new Date(year, month, 1);
+
+    const diaries = await findAllByAuthor(member.id, `${year}-${month}-01`,
+                                                end.toISOString().substring(0, 10));
+    return res.status(200).json({
+        data: diaries
+    });
+};
+
+exports.diary = async (req, res, next) => {
+    const member = res.locals.decoded;
+    const year = parseInt(req.query.year, 10);
+    const month = parseInt(req.query.month, 10) - 1;
+    const day = parseInt(req.query.day, 10);
+    const date = new Date(year, month - 1, day + 1, 0, 0, 0, 0);
+    const diary = await findDiary(member.id, date.toISOString().substring(0, 10));
+    return res.status(200).json({
+        diaryNo: diary.diaryNo,
+        authorId: diary.authorId,
+        content: diary.content,
+        emoji: diary.emoji,
+        wroteDate: diary.wroteDate
+    });
+};
+
+exports.findDiary = async (req, res, next) => {
+    const diary = await findByNo(req.params.no);
+    return res.status(200).json({
+        diaryNo: diary.diaryNo,
+        authorId: diary.authorId,
+        content: diary.content,
+        emoji: diary.emoji,
+        wroteDate: diary.wroteDate
+    });
+}
+
+exports.saveDiary = (req, res, next) => {
+    const { content, wrote_at, emoji } = req.body;
+    const date = new Date(wrote_at);
+    const member = res.locals.decoded;
+
+    const wrote = new Date(date.getFullYear(), date.getMonth() - 1, date.getDate() + 1);
+    save(v4(), member.id, content, emoji, wrote.toISOString().substring(0, 10));
+    return res.status(201).json({
+        message: "good"
+    });
+};
+
+exports.updateDiary = (req, res, next) => {
+    const { content, emoji } = req.body;
+    const member = res.locals.decoded;
+    const no = req.params.no;
+
+    if (exists(member.id, no)) {
+        update(no, content, emoji);
+        return res.status(200).json({
+            message: "complete"
+        });
+    }
+    else {
+        return res.status(404).json({
+            message: "cannot found diary"
+        });
+    }
+};
+
+exports.deleteDiary = (req, res, next) => {
+    const member = res.locals.decoded;
+    const no = req.query.no;
+
+    if (exists(member.id, no)) {
+        deleteByNo(no);
+        return res.status(204).json({});
+    }
+    else {
+        return res.status(404).json({
+            message: "cannot found diary"
+        });
+    }
+};
